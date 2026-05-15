cf8790ef0052b0714294c26072afc90bdfcf1e2b
Pg28fyiW <131163094+Pg28fyiW@users.noreply.github.com>
2026-05-15 10:11:03 +0900

    Add files via upload

---
+++ test.js
@@ -0,0 +1,45 @@
+const subsidyTypeList = [
+    {
+        id: "test_radio_1",
+        value: "facility",
+        label: "ラジオボタン１",
+        checked: true
+    },
+    {
+        id: "test_radio_2",
+        value: "service",
+        label: "ラジオボタン２",
+        checked: false
+    }
+];
+
+const radioGroup = document.getElementById("radioGroup");
+
+subsidyTypeList.forEach(function (item) {
+    const div = document.createElement("div");
+
+    const input = document.createElement("input");
+    input.type = "radio";
+    input.id = item.id;
+    input.name = "tokyoSubsidyType";
+    input.value = item.value;
+
+    if (item.checked) {
+        input.checked = true;
+    }
+
+    const label = document.createElement("label");
+    label.className = "radio_button";
+    label.htmlFor = item.id;
+    label.textContent = item.label;
+
+    const img = document.createElement("img");
+    img.src = "question.png";
+    img.className = "question pointer";
+
+    div.appendChild(input);
+    div.appendChild(label);
+    div.appendChild(img);
+
+    radioGroup.appendChild(div);
+});
